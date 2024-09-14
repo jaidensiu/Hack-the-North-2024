4719315e7021bfe7330e6dc63618d683c4ece698
terwo <105677497+terwo@users.noreply.github.com>
2024-09-14 19:30:44 -0400

    Add provider to layout, and create provider for context

--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,17 +1,22 @@
-import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React, { useEffect } from "react";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-reanimated";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { UserProvider } from "../contexts/UserContext"; // Import the UserProvider
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -24,17 +29,15 @@ export default function RootLayout() {
     return null;
   }
 
-  const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-  unsavedChangesWarning: false,
-});
-
   return (
     <ThemeProvider value={DefaultTheme}>
       <ConvexProvider client={convex}>
-      <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)"/>
-          <Stack.Screen name="+not-found" />
-        </Stack>
+        <UserProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </UserProvider>
       </ConvexProvider>
     </ThemeProvider>
   );
