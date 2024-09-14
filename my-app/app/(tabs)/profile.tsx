a6762b15815b7b88fa7425bc3406f9173f18e7b1
jaidensiu <jaidensiu@gmail.com>
2024-09-14 12:46:25 -0400

    chore: mocked profile tab

--- my-app/app/(tabs)/profile.tsx
+++ my-app/app/(tabs)/profile.tsx
@@ -3,23 +3,37 @@ import { StyleSheet } from 'react-native';
 import ScrollView from '@/components/ScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import ProfileCard from '@/components/ProfileCard';
 
-export default function ProfileScreen() {
+
+// TODO: interface props are for defining models
+interface ProfileScreenProps {
+  name: string;
+  phone: string;
+  email: string;
+  rating: number;
+}
+
+const ProfileScreen: React.FC<ProfileScreenProps> = ({ name, phone, email, rating }) => {
+  const user = {
+    name: 'Franklin Ma',
+    role: 'Teacher',
+    phone: '123-456-7890',
+    email: 'franklinma@gmail.com',
+    rating: 4.9,
+  };
+
   return (
     <>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Your Profile</ThemedText>
       </ThemedView>
-      <ScrollView
-        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}>
-        <ThemedView>
-          <ThemedText>item0</ThemedText>
-          <ThemedText>item1</ThemedText>
-          <ThemedText>item2</ThemedText>
-          <ThemedText>item3</ThemedText>
-          <ThemedText>item4</ThemedText>
-          <ThemedText>item5</ThemedText>
-        </ThemedView>
+      <ScrollView headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}>
+        <ProfileCard title="Name" value={user.name} />
+        <ProfileCard title="Role" value={user.role} />
+        <ProfileCard title="Phone" value={user.phone} />
+        <ProfileCard title="Email" value={user.email} />
+        <ProfileCard title="Rating" value={user.rating} />
       </ScrollView>
     </>
   );
@@ -34,3 +48,5 @@ const styles = StyleSheet.create({
     paddingBottom: 8
   },
 });
+
+export default ProfileScreen;
